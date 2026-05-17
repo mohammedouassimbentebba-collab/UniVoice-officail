c995a33f3dcbc2007c1bba6877ef158e49d72c2a
mohammedouassimbentebba-collab <mohammedouassim.bentebba@etu.univ-batna2.dz>
2026-05-17 19:14:09 +0100

    Add files via upload

---
+++ UniVoice-Final/config.js
@@ -0,0 +1,4 @@
+// Generated — do not commit this file.
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://ilxdnwapvrpofxnurboe.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_TXbv2AjCh5IimCbTRXZ2hw_hZUMqo6K"};
